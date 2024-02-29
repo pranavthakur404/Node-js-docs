49be9bb1caee502b2ee421a494a0a990341a8c8d
pranavthakur404 <pranavufc@gmail.com>
2024-02-29 13:42:34 +0530

    chapter1 code

--- ch1/index.js
+++ ch1/index.js
@@ -1,1 +1,23 @@
-console.log("hello");
+//******************* */ HOW TO IMPORT MODULES USING REQURE JS ***************************
+// const lib = require("./lib");
+// console.log(lib.sum(5, 6));
+
+//******************* */ FILE SYSTEM MODULE ***************************
+// const fs = require("fs");
+
+// const text = fs.readFile("./demo.txt", "utf-8", (err,text) => {
+//   console.log(text);
+// });
+
+
+//******************* */ Example for understanding live server issue and using nodemon ***************************
+// const express = require("express");
+
+// console.log("hello");
+
+// const server = express();
+// server.listen(8000);
+
+
+
+// ********************   npm update, outdate, (^, ~, *) ********************
